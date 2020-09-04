4aeae54a3466c076fcdfca96e949ed6c0cfdec8d
kenton7 <63309306+kenton7@users.noreply.github.com>
2020-09-05 00:37:09 +0300

    пофиксил проблему, когда чарт не обновлялся при добавлении новых данных без перезапуска приложения

---
+++ Water+/Models/Localization.swift
@@ -0,0 +1,13 @@
+//
+//  Localization.swift
+//  Water+
+//
+//  Created by Илья Кузнецов on 02.09.2020.
+//  Copyright © 2020 Ilya Kuznetsov. All rights reserved.
+//
+
+import UIKit
+
+class Localization: NSObject {
+
+}
---
+++ Water+/Views/NotificationsCell.swift
@@ -0,0 +1,24 @@
+//
+//  NotificationsCell.swift
+//  Water+
+//
+//  Created by Илья Кузнецов on 31.08.2020.
+//  Copyright © 2020 Ilya Kuznetsov. All rights reserved.
+//
+
+import UIKit
+
+class NotificationsCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
